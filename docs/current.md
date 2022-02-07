4c610476f111c4ed506b5c65ba83b4be8d35061e
broxigarette <edeltitnu@gmail.com>
2022-02-07 20:53:49 +0300

    first commit

---
+++ docs/current.md
@@ -0,0 +1,7 @@
+# Staple VM Spec
+
+## Instgraction 
+
+PUSH    - Push an 8 bit nuber onto the stack
+ADD     - Pops last two number
+HLT     - Pops stack and returns result 
---
+++ src/util.c
@@ -0,0 +1,27 @@
+#include "../include/util.h"
+
+char *read_ascii_file(const char *path) 
+{
+    // Open file
+    FILE *file = fopen(path, "r");
+    if (!file) {
+        printf("Could not open file %s\n", path);
+        return NULL;
+    }
+
+    fseek(file, 0, SEEK_END);
+    int size = ftell(file);
+    fseek(file, 0, SEEK_SET);
+
+    char *buf = (char*) malloc(sizeof(char) * (size + 1));
+    if (!buf) {
+        printf("Could not allocated memory for file\n");
+        return NULL;
+    }
+    fread(buf, 1, size, file);
+    buf[size] = '\0';
+
+    fclose(file);
+
+    return buf;
+}
